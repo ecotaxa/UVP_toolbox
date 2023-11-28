5c762c200fe4835b988ea02f7eca4f9f3ba4c0aa
ccatalano <camille.catalano@obs-vlfr.fr>
2023-11-28 09:50:59 +0100

    create sample: robustness of read seaexplorer file

--- UVP6_samples_creation/FillCTDtableSeaexplorer.m
+++ UVP6_samples_creation/FillCTDtableSeaexplorer.m
@@ -13,19 +13,44 @@ function [ctd_table] = FillCTDtableSeaexplorer(ctd_table, data_table)
 
 ctd_table.("qc flag") = zeros(height(data_table),1);
 ctd_table.("time [yyyymmddhhmmssmmm]") = datestr(data_table{:,1}, 'yyyymmddHHMMSSFFF');
-ctd_table.("chloro fluo [mg chl m-3]") = data_table.FLBBCD_CHL_SCALED; %%%%% µg/L pas verifie mais a priori ok
-ctd_table.("fcdom [ppb qse]") = data_table.FLBBCD_CDOM_SCALED; %%%%%%%%%%%%% ppb ok
+
+
+%% catch glider data
 try
-    oxy = 0.001 * data_table.AROD_FT_DO' / sw_dens(data_table.LEGATO_SALINITY', data_table.LEGATO_TEMPERATURE', data_table.LEGATO_PRESSURE');
-    %ctd_table.("oxygen [umol kg-1]") =  oxy'; %%%%%%%%%% AROD_FT_DO in µmol/L conversion faite, mais correction à faire !!!!
-    ctd_table.("conductivity [ms cm-1]") = data_table.LEGATO_CONDUCTIVITY; %%%%%%%% mS/cm ok
-    ctd_table.("temperature [degc]") = data_table.LEGATO_TEMPERATURE; %%%%%%%%% °C ok
     ctd_table.("pressure [db]") = data_table.LEGATO_PRESSURE; %%%%%%%%%%%%%%% db ok
-    ctd_table.("practical salinity [psu]") = data_table.LEGATO_SALINITY; %%%%%%% ups a priori ok, peut etre a verifier
 catch
     ctd_table.("pressure [db]") = data_table.NAV_DEPTH;
 end
 
+try
+    ctd_table.("chloro fluo [mg chl m-3]") = data_table.FLBBCD_CHL_SCALED; %%%%% µg/L pas verifie mais a priori ok
+catch
+    disp("No fluorescence data in the glider file")
+end
+
+try
+    ctd_table.("fcdom [ppb qse]") = data_table.FLBBCD_CDOM_SCALED; %%%%%%%%%%%%% ppb ok
+catch
+    disp("No fcdom data in the glider file")
+end
+
+try
+    ctd_table.("practical salinity [psu]") = data_table.LEGATO_SALINITY; %%%%%%% ups a priori ok, peut etre a verifier
+    ctd_table.("temperature [degc]") = data_table.LEGATO_TEMPERATURE; %%%%%%%%% °C ok
+    oxy = 0.001 * data_table.AROD_FT_DO' / sw_dens(data_table.LEGATO_SALINITY', data_table.LEGATO_TEMPERATURE', data_table.LEGATO_PRESSURE');
+    %ctd_table.("oxygen [umol kg-1]") =  oxy'; %%%%%%%%%% AROD_FT_DO in µmol/L conversion faite, mais correction à faire !!!!
+catch
+    disp("No salinity in the glider file")
+end
+
+try
+    ctd_table.("conductivity [ms cm-1]") = data_table.LEGATO_CONDUCTIVITY; %%%%%%%% mS/cm ok
+catch
+    disp("No conductivity in the glider file")    
+end
+
+
+%% fill the table
 data_table(:,14:21) = [];
 ctd_table = [ctd_table, data_table(:,3:end-1)];
 
