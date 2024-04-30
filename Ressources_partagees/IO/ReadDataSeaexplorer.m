24e3b8c03145ed65019ed31c0a53f7deb4568cb9
ccatalano <camille.catalano@obs-vlfr.fr>
2024-04-30 15:13:41 +0200

    correct bug : read datetime seaexplorer

--- Ressources_partagees/IO/ReadDataSeaexplorer.m
+++ Ressources_partagees/IO/ReadDataSeaexplorer.m
@@ -14,8 +14,12 @@ function [meta_data, data_table] = ReadDataSeaexplorer(filepathgz)
 %
 filepath = gunzip(filepathgz);
 %data_table = readtable(filepath{1}, 'FileType', 'text', 'Format', '%{dd/MM/uuuu HH:mm:ss.SSS}D %f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f');
-data_table = readtable(filepath{1}, 'FileType', 'text');
-data_table = convertvars(data_table, 'PLD_REALTIMECLOCK', 'datetime');
+datetime.setDefaultFormats('default','dd/MM/uuuu HH:mm:ss.SSS');
+opts = detectImportOptions(filepath{1}, 'FileType', 'text');
+opts = setvaropts(opts, 'PLD_REALTIMECLOCK', 'InputFormat', 'dd/MM/uuuu HH:mm:ss.SSS');
+data_table = readtable(filepath{1}, opts);
+%data_table = readtable(filepath{1}, 'FileType', 'text');
+%data_table = convertvars(data_table, 'PLD_REALTIMECLOCK', 'datetime');
 meta_data = [datenum(data_table.PLD_REALTIMECLOCK) data_table.NAV_DEPTH data_table.NAV_LATITUDE data_table.NAV_LONGITUDE];
 delete(filepath{1});
 end
