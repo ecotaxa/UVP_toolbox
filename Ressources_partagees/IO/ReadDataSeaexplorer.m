b3874820e03af3ab2af800a9a345414bb9373ee4
ccatalano <camille.catalano@imev-mer.fr>
2025-07-02 17:25:46 +0200

    debug read seaexplorer

--- Ressources_partagees/IO/ReadDataSeaexplorer.m
+++ Ressources_partagees/IO/ReadDataSeaexplorer.m
@@ -17,9 +17,11 @@ filepath = gunzip(filepathgz);
 datetime.setDefaultFormats('default','dd/MM/uuuu HH:mm:ss.SSS');
 opts = detectImportOptions(filepath{1}, 'FileType', 'text');
 opts = setvaropts(opts, 'PLD_REALTIMECLOCK', 'InputFormat', 'dd/MM/uuuu HH:mm:ss.SSS');
+opts = setvartype(opts, {'NAV_DEPTH', 'NAV_LATITUDE', 'NAV_LONGITUDE'}, 'double');
 data_table = readtable(filepath{1}, opts);
 %data_table = readtable(filepath{1}, 'FileType', 'text');
 %data_table = convertvars(data_table, 'PLD_REALTIMECLOCK', 'datetime');
+disp(filepathgz)
 meta_data = [datenum(data_table.PLD_REALTIMECLOCK) data_table.NAV_DEPTH data_table.NAV_LATITUDE data_table.NAV_LONGITUDE];
 delete(filepath{1});
 end
